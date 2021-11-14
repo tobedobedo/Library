e185c8a586b2188d31f7229a266380199b2adef1
irina <fomenkova.i17@gmail.com>
2021-11-14 12:37:30 +0300

    #1

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\AuthorService;
+use App\Service\BookService;
+use Exception;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthorController extends AbstractController
+{
+    /**
+     * @Route("/api/author", name="author")
+     */
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthorService $authorService
+     * @return JsonResponse
+     * @Route("/api/create-new-author")
+     */
+    public function createAuthor(Request $request, AuthorService $authorService): JsonResponse
+    {
+        $json = $request->getContent();
+        $body = json_decode($json, true);
+
+        try {
+            if (isset($body['name']) && isset($body['surname'])) {
+                $authorService->createNewAuthor($body['name'], $body['surname'], $body['idBook'] ?? null);
+            } else {
+                throw new RuntimeException('Недостаточно данных для записи автора', 400);
+            }
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ],
+                $e->getCode());
+        }
+
+        return new JsonResponse(['success' => true]);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthorService $authorService
+     * @return JsonResponse
+     * @Route("/api/edit-author")
+     */
+    public function editAuthor(Request $request, AuthorService $authorService): JsonResponse
+    {
+        $json = $request->getContent();
+        $body = json_decode($json,true);
+
+        try {
+            if (isset($body['idAuthor'])) {
+                $authorService->editAuthor($body['idAuthor'], $body['name'] ?? null, $body['surname'] ?? null, $body['idBook'] ?? null);
+            } else {
+                throw new RuntimeException('Идентификатор автора не указан', 400);
+            }
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ],
+            $e->getCode());
+        }
+
+        return new JsonResponse(['success' => true]);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthorService $authorService
+     * @return JsonResponse
+     * @Route("/api/remove-author")
+     */
+    public function removeAuthorsByNameAndSurname(Request $request, AuthorService $authorService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        try {
+            $authorService->removeAuthorsByNameAndSurname($data);
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return new JsonResponse(['success' => true]);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthorService $authorService
+     * @return JsonResponse
+     * @Route("/api/get-authors")
+     */
+    public function getAuthors(Request $request, AuthorService $authorService): JsonResponse
+    {
+        try {
+            $json = $request->getContent();
+            $body = json_decode($json, true);
+
+            return new JsonResponse($authorService->getAuthors($body ?? []));
+
+        } catch (Exception $e) {
+            return new JsonResponse([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+}
---
+++ src/Service/AuthorService.php
@@ -0,0 +1,100 @@
+<?php
+
+
+namespace App\Service;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Repository\AuthorRepository;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use RuntimeException;
+
+class AuthorService
+{
+    private EntityManagerInterface $em;
+    private AuthorRepository $authorRepository;
+
+    /**
+     * BookService constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        /** @var EntityManager em */
+        $this->em = $em;
+        $this->authorRepository = $em->getRepository(Author::class);
+    }
+
+    public function createNewAuthor(string $name, string $surname, ?int $idBook)
+    {
+        if ($idBook >= 1){
+
+            /** @var BookRepository $repository */
+            $repository = $this->em->getRepository(Book::class);
+            $book = $repository->find($idBook);
+
+        }
+
+        $author = new Author();
+        $author->setName($name);
+        $author->setSurname($surname);
+        $author->addBook($book ?? null);
+
+        $this->em->persist($author);
+        $this->em->flush();
+
+    }
+    public function editAuthor(int $idAuthor, ?string $name, ?string $surname, ?int $idBook)
+    {
+        $author = $this->authorRepository->find($idAuthor);
+
+        if ($author != null) {
+
+            if ($name != null) {
+                $author->setName($name);
+            }
+
+            if ($surname != null) {
+                $author->setSurname($surname);
+            }
+
+            if($idBook != null){
+                /** @var BookRepository $repositoryBook */
+                $repositoryBook = $this->em->getRepository(Book::class);
+                $book= $repositoryBook->find($idBook);
+                $author->addBook($book);
+            }
+
+            $this->em->flush();
+        } else {
+            throw new RuntimeException('Автор не найден', 400);
+        }
+    }
+
+    public function removeAuthorsByNameAndSurname(array $data)
+    {
+        if (isset($data['name']) && isset($data['surname'])) {
+            $authors = $this->authorRepository->findBy([
+                'name' => $data['name'],
+                'surname' => $data['surname']
+            ]);
+        }
+
+        if (isset($authors) && $authors != null) {
+            foreach ($authors as $author) {
+                $this->em->remove($author);
+            }
+
+            $this->em->flush();
+        }
+    }
+
+    public function getAuthors(array $data): array
+    {
+        /** @var AuthorRepository $repository */
+        $repository = $this->em->getRepository(Author::class);
+        return $repository->findByFilters($data);
+    }
+}
